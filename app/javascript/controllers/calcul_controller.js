765794b2abd90ac21f5a023bf1b8acf0d4cdcf94
FlorianFaneuil <florian.faneuil@gmail.com>
2023-08-24 18:34:43 +0200

    v

--- app/javascript/controllers/calcul_controller.js
+++ app/javascript/controllers/calcul_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calcul"
 export default class extends Controller {
-  static targets = ["begin_date", "end_date"]
+  static targets = ["begin_date", "end_date", "price_per_day", "total_price"]
 
   connect() {
     console.log(this.begin_dateTarget.value)
@@ -10,10 +10,20 @@ export default class extends Controller {
   refresh(){
   console.log(this.begin_dateTarget.value)
   console.log(this.end_dateTarget.value)
-  console.log((this.end_dateTarget) - (this.begin_dateTarget).value)
   }
+
   total_price(){
+    const beginDate = new Date(this.begin_dateTarget.value);
+    const endDate = new Date(this.end_dateTarget.value);
+    const price_per_day = parseFloat(this.data.get("price_per_date"));
 
+    if (!isNaN(beginDate) && !isNaN(endDate) && price_per_day) {
+      const timeDiff = endDate - beginDate;
+      const daysDiff = timeDiff / (1000 * 3600 * 24);
+      const totalPrice = daysDiff * price_per_day;
+
+      this.total_priceTarget.innerHTML = `<i class="fa-solid fa-money-bill-wave" style="color: #F8C933;"></i> ${totalPrice.toFixed(2)} €`;
+
+    }
   }
-
 }
