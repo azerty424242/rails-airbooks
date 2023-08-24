991e51f3586ab0c0f90ad8c1e483780a14085861
Maxime Montfort consultant.blockchain@outlook.com <consultant.blockchain@outlook.com>
2023-08-24 12:58:22 +0200

    Bookmark model OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_134340) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_105555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_134340) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_bookmarks_on_book_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -84,6 +93,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_134340) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "books"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "books", "users", column: "owner_id"
   add_foreign_key "reservations", "books"
   add_foreign_key "reservations", "users", column: "renter_id"
